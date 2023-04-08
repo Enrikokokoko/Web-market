62a3f5a8e2cb99165bf287a09a7c615a5c06f171
Enriko Raymundu <116963958+Enrikokokoko@users.noreply.github.com>
2023-04-08 23:19:16 +0300

    User module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './Schema/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature(
+      [{ name: User.name, schema: UserSchema }],
+      'user',
+    ),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
